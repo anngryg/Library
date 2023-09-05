f5f604cdc022e88d9d7ad43e634faea3b66130c7
Anna <anna.grygierczyk@wp.pl>
2023-09-05 20:38:46 +0200

    book list in a form of a table, resetting every submit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Adding modal functions
 const openModalButton = document.querySelector("[data-modal-target]");
 const closeModalButton = document.querySelector("[data-close-button]");
 const overlay = document.getElementById("overlay");
@@ -9,6 +10,7 @@ openModalButton.addEventListener("click", () => {
 
 closeModalButton.addEventListener("click", () => {
   const modal = closeModalButton.closest(".modal");
+  resetInputs();
   closeModal(modal);
 });
 
@@ -30,26 +32,71 @@ function closeModal(modal) {
   }
 }
 
-let submitBtn = document.querySelector("#submit");
-
-submitBtn.addEventListener("click", addBookToLibrary);
-
+//
 let usersAuthor = document.querySelector("#author");
 let usersTitle = document.querySelector("#title");
+let usersPages = document.querySelector("#pages");
+let userBookList = document.querySelector("tbody");
+
+let submitBtn = document.querySelector("#submit");
+
+submitBtn.addEventListener("click", () => {
+  const modal = closeModalButton.closest(".modal");
+  addBookToLibrary();
+  resetInputs();
+  closeModal(modal);
+  createBookList();
+});
 
 const myLibrary = [];
 
-function Book(title, author, read) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
+  this.pages = pages;
   this.read = read;
 }
 
 function addBookToLibrary() {
-  myLibrary.push(new Book(usersAuthor.value, usersTitle.value, "not"));
+  if (
+    usersTitle.value.length === 0 ||
+    usersAuthor.value.length === 0 ||
+    usersPages.value.length === 0
+  ) {
+    alert("Please fill out the fields!");
+  } else {
+    myLibrary.push(
+      new Book(usersTitle.value, usersAuthor.value, usersPages.value, "not")
+    );
+  }
 }
 
-//    this.info = function(){
-//   return title+" by "+author+", "+pages+", "+read;}
+function resetInputs() {
+  usersAuthor.value = "";
+  usersTitle.value = "";
+  usersPages.value = "";
+}
 
-console.log(myLibrary);
+function createBookList() {
+  clearList();
+  for (let i = 0; i < myLibrary.length; i++) {
+    let newBook = document.createElement("tr");
+    userBookList.appendChild(newBook);
+
+    let newTitle = document.createElement("td");
+    newTitle.innerHTML = myLibrary[i].title;
+    newBook.appendChild(newTitle);
+
+    let newAuthor = document.createElement("td");
+    newAuthor.innerHTML = myLibrary[i].author;
+    newBook.appendChild(newAuthor);
+
+    let newPages = document.createElement("td");
+    newPages.innerHTML = myLibrary[i].pages;
+    newBook.appendChild(newPages);
+  }
+}
+
+function clearList() {
+  userBookList.innerHTML = "";
+}
